10cbaa39ab4d158434fe6fff847c256a7ad166fb
Maxim <farnaks.net@gmail.com>
2026-03-15 11:38:24 +0200

    fix: comments

--- backend/main.py
+++ backend/main.py
@@ -27,13 +27,10 @@ app.add_middleware(
 
 @app.get("/api/neares-post")
 def api_get_neares_post(x: Optional[float] = None, y: Optional[float] = None, db: Session = Depends(get_db)):
-    # `getNearesPost` expects parameters (lat, len, db).
     return getNearesPost(x, y, db)
 
 @app.get("/api/plume")
 def api_get_plume(lat: float, lng: float, wind_dir: float, wind_speed: float):
-    # У реальному житті ти можеш передавати лише ID станції і діставати ці дані з БД.
-    # Для прикладу ми приймаємо їх прямо з фронтенду.
     return generate_plume_image(lat, lng, wind_dir, wind_speed)
 
 @app.get("/api/temperature-plume")
